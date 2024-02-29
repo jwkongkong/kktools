3dcbb427195c47242f6e3249a4dfcac77b86c0e4
wukong <wukong@dian.so>
2024-02-29 17:28:15 +0800

    fix react.

--- dist/components/empty.js
+++ dist/components/empty.js
@@ -5,17 +5,22 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = EmptyCard;
 var _react = require("@phosphor-icons/react");
+var _jsxRuntime = require("react/jsx-runtime");
 function EmptyCard() {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "text-center py-10"
-  }, /*#__PURE__*/React.createElement("div", {
-    className: "w-full flex justify-center items-center  space-x-4"
-  }, /*#__PURE__*/React.createElement(_react.Coffee, {
-    size: 30,
-    className: "text-gray-600 font-light"
-  }), " ", /*#__PURE__*/React.createElement("div", {
-    className: "text-2xl text-gray-600 font-light "
-  }, "\u6682\u65E0\u6570\u636E")), /*#__PURE__*/React.createElement("div", {
-    className: "text-gray-600 mt-3 font-light"
-  }, "No Content To Show"));
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+    className: "text-center py-10",
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+      className: "w-full flex justify-center items-center  space-x-4",
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_react.Coffee, {
+        size: 30,
+        className: "text-gray-600 font-light"
+      }), " ", /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+        className: "text-2xl text-gray-600 font-light ",
+        children: "\u6682\u65E0\u6570\u636E"
+      })]
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+      className: "text-gray-600 mt-3 font-light",
+      children: "No Content To Show"
+    })]
+  });
 }
